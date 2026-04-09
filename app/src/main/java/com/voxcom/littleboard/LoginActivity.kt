716d1135cf272802b5f29999ead17bec899eedeb
Midhun KP <midhumidhun342@gmail.com>
2026-04-09 12:53:23 +0530

    Add : Screen navigation

--- app/src/main/java/com/voxcom/littleboard/LoginActivity.kt
+++ app/src/main/java/com/voxcom/littleboard/LoginActivity.kt
@@ -1,20 +1,23 @@
 package com.voxcom.littleboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+    private  lateinit var login : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        login = findViewById(R.id.loginBtn)
+        login.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/voxcom/littleboard/SplashActivity.kt
+++ app/src/main/java/com/voxcom/littleboard/SplashActivity.kt
@@ -1,17 +1,25 @@
 package com.voxcom.littleboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
-
+    private lateinit var splash : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        splash = findViewById(R.id.splashBtn)
         setContentView(R.layout.activity_splash)
+
+        splash.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
