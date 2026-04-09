a3d91e715e130c6c2c2015d61deb32b941528cbf
Midhun KP <midhumidhun342@gmail.com>
2026-04-09 12:55:31 +0530

    fix : change type

--- app/src/main/java/com/voxcom/littleboard/SplashActivity.kt
+++ app/src/main/java/com/voxcom/littleboard/SplashActivity.kt
@@ -3,13 +3,14 @@ package com.voxcom.littleboard
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var splash : Button
+    private lateinit var splash : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
